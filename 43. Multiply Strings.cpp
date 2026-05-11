085b40dff1c9e4c1a23140246ed9e89746e4e0f7
Anisha Chhajer <anishachhajer16@gmail.com>
2026-05-11 22:22:35 +0530

    multiply string

--- 43. Multiply Strings.cpp
+++ 43. Multiply Strings.cpp
@@ -1,59 +1,59 @@
-// #include <iostream>
-// #include <vector>
-// using namespace std;
+#include <iostream>
+#include <vector>
+using namespace std;
 
-// class Solution {
-// public:
-//     string multiply(string num1, string num2) {
+class Solution {
+public:
+    string multiply(string num1, string num2) {
 
-//         // If any number is 0
-//         if (num1 == "0" || num2 == "0") {
-//             return "0";
-//         }
+        // If any number is 0
+        if (num1 == "0" || num2 == "0") {
+            return "0";
+        }
 
-//         int n = num1.size();
-//         int m = num2.size();
+        int n = num1.size();
+        int m = num2.size();
 
-//         vector<int> ans(n + m, 0);
+        vector<int> ans(n + m, 0);
 
-//         // Multiply from back
-//         for (int i = n - 1; i >= 0; i--) {
+        // Multiply from back
+        for (int i = n - 1; i >= 0; i--) {
 
-//             for (int j = m - 1; j >= 0; j--) {
+            for (int j = m - 1; j >= 0; j--) {
 
-//                 int mul = (num1[i] - '0') * (num2[j] - '0');
+                int mul = (num1[i] - '0') * (num2[j] - '0');
 
-//                 int sum = mul + ans[i + j + 1];
+                int sum = mul + ans[i + j + 1];
 
-//                 ans[i + j + 1] = sum % 10;
+                ans[i + j + 1] = sum % 10;
 
-//                 ans[i + j] = ans[i + j] + sum / 10;
-//             }
-//         }
+                ans[i + j] = ans[i + j] + sum / 10;
+            }
+        }
 
-//         string result = "";
+        string result = "";
 
-//         // Convert vector to string
-//         for (int x : ans) {
+        // Convert vector to string
+        for (int x : ans) {
 
-//             // Skip starting zeros
-//             if (!(result == "" && x == 0)) {
-//                 result = result + to_string(x);
-//             }
-//         }
+            // Skip starting zeros
+            if (!(result == "" && x == 0)) {
+                result = result + to_string(x);
+            }
+        }
 
-//         return result;
-//     }
-// };
+        return result;
+    }
+};
 
-// int main() {
+int main() {
 
-//     Solution s;
+    Solution s;
 
-//     string num1 = "123";
-//     string num2 = "45";
+    string num1 = "123";
+    string num2 = "45";
 
-//     cout << s.multiply(num1, num2);
+    cout << s.multiply(num1, num2);
 
-//     return 0;
-// }
+    return 0;
+}
