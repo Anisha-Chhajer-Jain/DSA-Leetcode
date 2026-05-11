33efda0cfff4c0cd2da7a17b134e3c42b4382645
Anisha Chhajer <anishachhajer16@gmail.com>
2026-05-11 22:23:26 +0530

    climbing stairs

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -1,38 +1,38 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// class Solution {
-// public:
-//     int climbStairs(int n) {
+class Solution {
+public:
+    int climbStairs(int n) {
 
-//         // Base cases
-//         if (n <= 2) {
-//             return n;
-//         }
+        // Base cases
+        if (n <= 2) {
+            return n;
+        }
 
-//         int first = 1;
-//         int second = 2;
+        int first = 1;
+        int second = 2;
 
-//         // Find ways step by step
-//         for (int i = 3; i <= n; i++) {
+        // Find ways step by step
+        for (int i = 3; i <= n; i++) {
 
-//             int third = first + second;
+            int third = first + second;
 
-//             first = second;
-//             second = third;
-//         }
+            first = second;
+            second = third;
+        }
 
-//         return second;
-//     }
-// };
+        return second;
+    }
+};
 
-// int main() {
+int main() {
 
-//     Solution s;
+    Solution s;
 
-//     int n = 5;
+    int n = 5;
 
-//     cout << s.climbStairs(n);
+    cout << s.climbStairs(n);
 
-//     return 0;
-// }
+    return 0;
+}
