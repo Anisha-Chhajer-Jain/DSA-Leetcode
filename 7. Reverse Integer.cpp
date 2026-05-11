11a399bdad23d4c4383a2d7931c613819649cf00
Anisha Chhajer <anishachhajer16@gmail.com>
2026-05-11 22:15:44 +0530

    reverse integer

--- 7. Reverse Integer.cpp
+++ 7. Reverse Integer.cpp
@@ -1,21 +1,32 @@
+#include <iostream>
+using namespace std;
+
 class Solution {
 public:
     int reverse(int x) {
+
         int rev = 0;
 
         while (x != 0) {
-            int digit = x % 10;  
-            rev = rev * 10 + digit; 
-            x = x / 10;    
-            
-            if (rev > INT_MAX / 10 || rev < INT_MIN / 10) {
-                return 0;
-            }        
+
+            int digit = x % 10;   // take last digit
+
+            rev = rev * 10 + digit; // add digit to reverse number
+
+            x = x / 10; // remove last digit
         }
 
         return rev;
     }
 };
 
-            
-        
+int main() {
+
+    Solution s;
+
+    int x = 1234;
+
+    cout << "Reverse Number = " << s.reverse(x);
+
+    return 0;
+}
