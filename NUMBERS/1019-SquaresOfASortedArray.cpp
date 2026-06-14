2f529b35de5be9ad94a66d00170db3ca9c76a1c7
Anisha Chhajer <anishachhajer16@gmail.com>
2026-06-14 21:51:25 +0530

    1019 cpp

--- NUMBERS/1019-SquaresOfASortedArray.cpp
+++ NUMBERS/1019-SquaresOfASortedArray.cpp
@@ -1,49 +1,49 @@
-// #include <iostream>
-// #include <vector>
-// #include <string>
-// #include <climits>
-// using namespace std;
+#include <iostream>
+#include <vector>
+#include <string>
+#include <climits>
+using namespace std;
 
-// class Solution {
-// public:
-//     vector<string> commonChars(vector<string>& words) {
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
 
-//         vector<string> ans;
+        vector<string> ans;
 
-//         for(char ch = 'a'; ch <= 'z'; ch++) {
-//             int minCount = INT_MAX;
+        for(char ch = 'a'; ch <= 'z'; ch++) {
+            int minCount = INT_MAX;
 
-//             for(string word : words) {
-//                 int count = 0;
+            for(string word : words) {
+                int count = 0;
 
-//                 for(char c : word) {
-//                     if(c == ch)
-//                         count++;
-//                 }
+                for(char c : word) {
+                    if(c == ch)
+                        count++;
+                }
 
-//                 minCount = min(minCount, count);
-//             }
+                minCount = min(minCount, count);
+            }
 
-//             while(minCount--) {
-//                 ans.push_back(string(1, ch));
-//             }
-//         }
+            while(minCount--) {
+                ans.push_back(string(1, ch));
+            }
+        }
 
-//         return ans;
-//     }
-// };
+        return ans;
+    }
+};
 
-// int main() {
-//     Solution obj;
+int main() {
+    Solution obj;
 
-//     vector<string> words = {"bella", "label", "roller"};
+    vector<string> words = {"bella", "label", "roller"};
 
-//     vector<string> result = obj.commonChars(words);
+    vector<string> result = obj.commonChars(words);
 
-//     cout << "Output: ";
-//     for(string s : result) {
-//         cout << s << " ";
-//     }
+    cout << "Output: ";
+    for(string s : result) {
+        cout << s << " ";
+    }
 
-//     return 0;
-// }
+    return 0;
+}
