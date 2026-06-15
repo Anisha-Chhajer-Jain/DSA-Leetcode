35512f063bd8dd18fde6a255d7cb25d64a0e9707
Anisha Chhajer <anishachhajer16@gmail.com>
2026-06-15 11:45:09 +0530

    gray code cpp

---
+++ b/21144.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int maxWords = 0;
+        for (int i = 0; i < sentences.size(); i++) {
+            int count = 1;
+            for (int j = 0; j < sentences[i].size(); j++) {
+                if (sentences[i][j] == ' ')
+                    count++;
+            }
+            maxWords = max(maxWords, count);
+        }
+        return maxWords;
+    }
+};
